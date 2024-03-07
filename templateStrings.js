4068219c7440b1bf38ca537a343ff89e2a971932
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-03-07 14:03:38 -0300

    feat: class on strings templates

---
+++ templateStrings.js
@@ -0,0 +1,12 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+const template = `
+    Olá
+    ${nome}`
+
+    console.log(concatenacao)
+    console.log(template)
+    console.log(`1 + 1 = ${1 + 1}`);
+
+    const up = texto => texto.toUpperCase()
+    console.log(`Ei... ${up('cuidado')}!`)
