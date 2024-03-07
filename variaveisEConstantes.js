c24ab37fe090aca25bcdd35ec317fb97a388bede
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-03-07 12:58:23 -0300

    feat: class on variables and constants

---
+++ variaveisEConstantes.js
@@ -0,0 +1,24 @@
+/*
+é possivel declarar uma var mais de uma vex no mesmo código
+não é possivel declarar let mais de uma vez
+não é possivel trocar valor de uma constante
+*/
+
+var a = 3 
+let b = 4
+
+var a = 30 
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+
+const c = 5
+// c = 50
+
+console.log(c)
