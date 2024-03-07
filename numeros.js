cc4ae38655c2e1b05123995a5fc59a88ac455fa8
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-03-07 13:10:44 -0300

    feat: class on number

---
+++ numeros.js
@@ -0,0 +1,17 @@
+const peso1 = 1.0
+const peso2 = Number('2.0')
+
+console.log(peso1, peso2)
+console.log(Number.isInteger(peso1))
+console.log(Number.isInteger(peso2))
+
+const avaliacao1 = 9.871
+const avaliacao2 = 6.871
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2
+const media = total / (peso1 + peso2)
+
+console.log(media.toFixed(2)) // somente duas casas decimais
+console.log(media.toString(2)) // em binario
+console.log(typeof media) // tipo da variavel
+console.log(typeof Number) // Number é função
