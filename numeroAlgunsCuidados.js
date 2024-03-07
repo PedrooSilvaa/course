f1a0d40693df62f64ea26cb7a3ac7bd3ce77eee2
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-03-07 13:21:00 -0300

    feat: class on number some care

---
+++ numeroAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0)
+console.log("10" / 2) //se tiver somente numero a string se transforma em numero e a operação funciona
+console.log("Show!" * 2) // NaN not a number
+console.log(0.1 + 0.7) //não da 0.8 por algumas restricoes da liguagem com ponto flutuante
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
