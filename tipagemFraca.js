cbdb0d8fb3a5e07f955cb72c3272596bb6bd8e11
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-03-07 13:01:59 -0300

    feat: class on weak typing

---
+++ tipagemFraca.js
@@ -0,0 +1,11 @@
+let qualquer = 'Legal'
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//evitar nome genericos e siglas
