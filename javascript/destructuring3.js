e8886eaf647a063a8c939927d673c437f5712231
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-03-09 18:51:18 -0300

    feat: class on destructuring

---
+++ javascript/destructuring3.js
@@ -0,0 +1,12 @@
+// numeros aleatorios com minimo e maximo
+
+function rand({ min = 0, max = 1000 }) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+const obj = { max: 50, min: 40 }
+console.log(rand(obj))
+console.log(rand({ min: 955 }))
+console.log(rand({}))
+console.log(rand()) // error
